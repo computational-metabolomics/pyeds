7c01dff6921954ed0a75c82b583fe1ff805cba63
dumz <m.du.1@bham.ac.uk>
2025-11-21 15:04:39 +0000

    feat: Filter based on mzCloud search

--- flatten_cd.py
+++ flatten_cd.py
@@ -34,41 +34,45 @@ with pyeds.EDS("HILIC_POS_5ppm.cdResult") as eds:
             ]
         )
         for level_1_item in items_iter:
-            # write level 1 content
-            csv_writer.writerow(
-                [
-                    level_1_item.Name,
-                    level_1_item.Formula,
-                    *[match_status_dictionary[status_number] for status_number in level_1_item.AnnotationMatchStatus],
-                    level_1_item.AnnotationDeltaMassInPPM,
-                    level_1_item.MolecularWeight,
-                    level_1_item.MassOverCharge,
-                    level_1_item.ReferenceIon,
-                ]
-            )
-            # write level 2 header
-            csv_writer.writerow(
-                [
-                    "",
-                    "Calc. MW",
-                    "Reference Ion",
-                    "RT [min]",
-                    "FWHM [min]",
-                ]
-            )
-            for level_2_item in level_1_item.Children:
-                # write level 2 content
+            if level_1_item.AnnotationMatchStatus[1] in (3, 4, 7):  # Filter based on mzCloud Search
+                # write level 1 content
+                csv_writer.writerow(
+                    [
+                        level_1_item.Name,
+                        level_1_item.Formula,
+                        *[
+                            match_status_dictionary[status_number]
+                            for status_number in level_1_item.AnnotationMatchStatus
+                        ],
+                        level_1_item.AnnotationDeltaMassInPPM,
+                        level_1_item.MolecularWeight,
+                        level_1_item.MassOverCharge,
+                        level_1_item.ReferenceIon,
+                    ]
+                )
+                # write level 2 header
                 csv_writer.writerow(
                     [
                         "",
-                        level_2_item.MolecularWeight,
-                        level_2_item.ReferenceIon,
-                        level_2_item.RetentionTime,
-                        level_2_item.FWHM,
+                        "Calc. MW",
+                        "Reference Ion",
+                        "RT [min]",
+                        "FWHM [min]",
                     ]
                 )
-                # write level 3 header
-                csv_writer.writerow(["", "", "Ion", "Charge"])
-                for level_3_item in level_2_item.Children:
-                    # write level 3 content
-                    csv_writer.writerow(["", "", level_3_item.IonDescription, level_3_item.Charge])
+                for level_2_item in level_1_item.Children:
+                    # write level 2 content
+                    csv_writer.writerow(
+                        [
+                            "",
+                            level_2_item.MolecularWeight,
+                            level_2_item.ReferenceIon,
+                            level_2_item.RetentionTime,
+                            level_2_item.FWHM,
+                        ]
+                    )
+                    # write level 3 header
+                    csv_writer.writerow(["", "", "Ion", "Charge"])
+                    for level_3_item in level_2_item.Children:
+                        # write level 3 content
+                        csv_writer.writerow(["", "", level_3_item.IonDescription, level_3_item.Charge])
